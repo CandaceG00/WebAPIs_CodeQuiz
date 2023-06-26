320aef74f8d2644620b37e865b2cc0d4265ba9d9
G <cmgostinski00@gmail.com>
2023-06-26 18:28:48 -0400

    added code to all three files

--- Assets/script.js
+++ Assets/script.js
@@ -1,90 +1,92 @@
 let containerQuestionEl = document.getElementById("question-container");
-let containerWelcomePageEl = document.getElementById("welcome-page");
-let containerEndPageEl = document.getElementById("end-page");
-let containerScoreEl = document.getElementById("score-banner");
-let formInitials = document.getElementById("initials-form");
-let containerHighScoresEl = document.getElementById("highscores");
-let ViewHighScoreEl = document.getElementById("view-high-scores");
-let listHighScoreEl = document.getElementById("high-score-list");
-let correctEl = document.getElementById("correct");
-let wrongEl = document.getElementById("wrong");
+      let containerStartEl = document.getElementById("starter-container");
+      let containerEndEl = document.getElementById("end-container")
+      let containerScoreEl = document.getElementById("score-banner")
+      let formInitials = document.getElementById("initials-form")
+      let containerHighScoresEl = document.getElementById("high-score-container")
+      let ViewHighScoreEl = document.getElementById("view-high-scores")
+      let listHighScoreEl = document.getElementById("high-score-list")
+      let correctEl = document.getElementById("correct")
+      let wrongEl = document.getElementById("wrong")
+      //buttons
+      let btnStartEl = document.querySelector("#start-game");
+      let btnGoBackEl = document.querySelector("#go-back")
+      let btnClearScoresEl = document.querySelector("#clear-high-scores")
+      //questions/answers element
+      let questionEl = document.getElementById("question")
+      let answerbuttonsEl = document.getElementById("answer-buttons")
+      let timerEl = document.querySelector("#timer");
+      let score = 0;
+      let timeleft;
+      let gameover
+      timerEl.innerText = 0;
 
-//buttons
-let btnStartEl = document.querySelector("#start-game");
-let btnGoBackEl = document.querySelector("#go-back");
-let btnClearScoresEl = document.querySelector("#clear-high-scores");
+      //High Score Array
+      let HighScores = [];
 
-//question/answers section
-let questionEl = document.getElementById("question");
-let answerOptionsEl = document.getElementById("answer-options");
-let timerEl = document.querySelector("#timer");
-var score = 0;
-var timeleft;
-var gameover;
-timerEl.innerText = 0;
+       //assign array details for questions 
+      let arrayShuffledQuestions
+      let QuestionIndex = 0
 
-//High Score Array
-let HighScores = [];
+    
+      
+      // The array of questions for our quiz game.
+      let questions = [
+        { q: 'Arrays in Javascript can be used to store __________.', 
+          a: '4. all of the above', 
+          choices: [{choice: '1. numbers'}, {choice: '2. booleans'}, {choice: '3. strings'}, {choice: '4. all of the above'}]
+        },
+        { q: 'Inside which HTML element do we put the javascript?', 
+          a: '3. <script>', 
+          choices: [{choice: '1. <h1>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <head>'}]
+        },
+        { q: 'In the code -- setinterval(time(),1000) -- what is time()?', 
+          a: '1. callback function', 
+          choices: [{choice: '1. callback function'}, {choice: '2. undefined'}, {choice: '3. variable'}, {choice: '4. all of the above'}]
+        },
+        { q: 'What syntax would call a function?', 
+          a: '4. function()', 
+          choices: [{choice: '1. var function'}, {choice: '2. function'}, {choice: '3. call function'}, {choice: '4. function()'}]
+        },
+        { q: 'When did javascript first appear?', 
+          a: '1. 1995', 
+          choices: [{choice: '1. 1995'}, {choice: '2. Roaring twenties'}, {choice: '3. 2005'}, {choice: '4. 2000'}]
+        },
+        { q: 'What does DOM stand for?', 
+          a: '2. Document Object Model', 
+          choices: [{choice: '1. Do Overnight Modules'}, {choice: '2. Document Object Model'}, {choice: '3. Divas Obviously Model'}, {choice: '4. Do Oo Mo'}]
+        },
+        { q: 'What is getItem commonly used for?', 
+          a: '2. local storage', 
+          choices: [{choice: '1. adding drama'}, {choice: '2. local storage'}, {choice: '3. online shopping'}, {choice: '4. naming a variable'}]
+        },
+      ];
+      
+        //if go back button is hit on high score page
+    let renderStartPage = function () {
+        containerHighScoresEl.classList.add("hide")
+        containerHighScoresEl.classList.remove("show")
+        containerStartEl.classList.remove("hide")
+        containerStartEl.classList.add("show")
+        containerScoreEl.removeChild(containerScoreEl.lastChild)
+        QuestionIndex = 0
+        gameover = ""
+        timerEl.textContent = 0 
+        score = 0
 
-//assign array details for questions 
-let arrayShuffledQuestions;
-let QuestionIndex = 0;
-
-
-var questions = [
-    { q: 'Arrays in Javascript can be used to store __________.', 
-      a: '4. all of the above', 
-      choices: [{choice: '1. numbers'}, {choice: '2. booleans'}, {choice: '3. strings'}, {choice: '4. all of the above'}]
-    },
-    { q: 'Inside which HTML element do we put the javascript?', 
-      a: '3. <script>', 
-      choices: [{choice: '1. <h1>'}, {choice: '2. <js>'}, {choice: '3. <script>'}, {choice: '4. <head>'}]
-    },
-    { q: 'In the code -- setinterval(time(),1000) -- what is time()?', 
-      a: '1. callback function', 
-      choices: [{choice: '1. callback function'}, {choice: '2. undefined'}, {choice: '3. variable'}, {choice: '4. all of the above'}]
-    },
-    { q: 'What syntax would call a function?', 
-      a: '4. function()', 
-      choices: [{choice: '1. var function'}, {choice: '2. function'}, {choice: '3. call function'}, {choice: '4. function()'}]
-    },
-    { q: 'When did javascript first appear?', 
-      a: '1. 1995', 
-      choices: [{choice: '1. 1995'}, {choice: '2. Roaring twenties'}, {choice: '3. 2005'}, {choice: '4. 2000'}]
-    },
-    { q: 'What does DOM stand for?', 
-      a: '2. Document Object Model', 
-      choices: [{choice: '1. Do Overnight Modules'}, {choice: '2. Document Object Model'}, {choice: '3. Divas Obviously Model'}, {choice: '4. Do Oo Mo'}]
-    },
-    { q: 'What is getItem commonly used for?', 
-      a: '2. local storage', 
-      choices: [{choice: '1. adding drama'}, {choice: '2. local storage'}, {choice: '3. online shopping'}, {choice: '4. naming a variable'}]
-    },
-  ];
-
-  let renderStartPage = function () {
-    containerHighScoresEl.classList.add("hide");
-    containerHighScoresEl.classList.remove("show");
-    containerWelcomePageEl.classList.remove("hide");
-    containerWelcomePageEl.classList.add("show");
-    containerScoreEl.removeChild(containerScoreEl.lastChild);
-    QuestionIndex = 0;
-    gameover = "";
-    timerEl.textContent = 0;
-    score = 0;
-
-    if (correctEl.className = "show") {
-        correctEl.classList.remove("show");
-        correctEl.classList.add("hide")
+        if (correctEl.className = "show") {
+            correctEl.classList.remove("show");
+            correctEl.classList.add("hide")
+        }
+        if (wrongEl.className = "show") {
+            wrongEl.classList.remove("show");
+            wrongEl.classList.add("hide");
+        }
     }
-    if (wrongEl.className = "show") {
-        wrongEl.classList.remove("show");
-        wrongEl.classList.add("hide");
-    }
-  }
 
-  let setTime = function () {
-    timeleft = 30;
+    //every second, check if game-over is true, or if there is time left. Start time at 30. 
+    let setTime = function () {
+        timeleft = 30;
 
     let timercheck = setInterval(function() {
         timerEl.innerText = timeleft;
@@ -101,61 +103,65 @@ var questions = [
         }
 
         }, 1000)
-  }
+    }
 
-  let startGame = function() {
-    //add classes to show/hide start and quiz screen
-    containerWelcomePageEl.classList.add('hide');
-    containerWelcomePageEl.classList.remove('show');
-    containerQuestionEl.classList.remove('hide');
-    containerQuestionEl.classList.add('show');
-    //Shuffle the questions so they show in random order
-    arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5)
-    setTime()
-    setQuestion()
-  }
+    let startGame = function() {
+        //add classes to show/hide start and quiz screen
+        containerStartEl.classList.add('hide');
+        containerStartEl.classList.remove('show');
+        containerQuestionEl.classList.remove('hide');
+        containerQuestionEl.classList.add('show');
+        //Shuffle the questions so they show in random order
+        arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5)
+        setTime()
+        setQuestion()
+      }
+    
+    //set next question for quiz
+    let setQuestion = function() {
+        resetAnswers()
+        displayQuestion(arrayShuffledQuestions[QuestionIndex])
+    }
 
-let setQuestion = function() {
-    resetAnswers()
-    displayQuestion(arrayShuffledQuestions[QuestionIndex])
-}
-
-let resetAnswers = function() {
-    while (answerOptionsEl.firstChild) {
-        answerOptionsEl.removeChild(answerOptionsEl.firstChild)
-    };
-};
-
-let displayQuestion = function(index) {
-    questionEl.innerText = index.q
-    for (var i = 0; i < index.choices.length; i++) {
-        let answerbutton = document.createElement('button')
-        answerbutton.innerText = index.choices[i].choice
-        answerbutton.classList.add('btn')
-        answerbutton.classList.add('answerbtn')
-        answerbutton.addEventListener("click", answerCheck)
-        answerOptionsEl.appendChild(answerbutton);
-        }
+    //remove answer buttons
+    let resetAnswers = function() {
+        while (answerbuttonsEl.firstChild) {
+            answerbuttonsEl.removeChild(answerbuttonsEl.firstChild)
+        };
     };
 
-let answerCorrect = function() {
-    if (correctEl.className === "hide") {
-        correctEl.classList.remove("hide")
-        correctEl.classList.add("banner")
-        wrongEl.classList.remove("banner")
-        wrongEl.classList.add("hide")
-        }
-    } 
-
-let answerWrong = function() {
-    if (wrongEl.className === "hide") {
-        wrongEl.classList.remove("hide")
-        wrongEl.classList.add("banner")
-        correctEl.classList.remove("banner")
-        correctEl.classList.add("hide")
+    //display question information (including answer buttons)
+    let displayQuestion = function(index) {
+        questionEl.innerText = index.q
+        for (var i = 0; i < index.choices.length; i++) {
+            let answerbutton = document.createElement('button')
+            answerbutton.innerText = index.choices[i].choice
+            answerbutton.classList.add('btn')
+            answerbutton.classList.add('answerbtn')
+            answerbutton.addEventListener("click", answerCheck)
+            answerbuttonsEl.appendChild(answerbutton)
+            }
+        };
+    //display correct! on screen
+    let answerCorrect = function() {
+        if (correctEl.className = "hide") {
+            correctEl.classList.remove("hide")
+            correctEl.classList.add("banner")
+            wrongEl.classList.remove("banner")
+            wrongEl.classList.add("hide")
+            }
+        }  
+    //display wrong! on screen
+    let answerWrong = function() {
+        if (wrongEl.className = "hide") {
+            wrongEl.classList.remove("hide")
+            wrongEl.classList.add("banner")
+            correctEl.classList.remove("banner")
+            correctEl.classList.add("hide")
         }
     }
 
+    //check if answer is correct    
     let answerCheck = function(event) {
         let selectedanswer = event.target
             if (arrayShuffledQuestions[QuestionIndex].a === selectedanswer.innerText){
@@ -169,6 +175,7 @@ let answerWrong = function() {
               timeleft = timeleft - 3;
           };
 
+        //go to next question, check if there is more questions
           QuestionIndex++
             if  (arrayShuffledQuestions.length > QuestionIndex + 1) {
                 setQuestion()
@@ -179,16 +186,18 @@ let answerWrong = function() {
                 }
     }
 
+        //Display total score screen at end of game
     let showScore = function () {
         containerQuestionEl.classList.add("hide");
-        containerEndPageEl.classList.remove("hide");
-        containerEndPageEl.classList.add("show");
+        containerEndEl.classList.remove("hide");
+        containerEndEl.classList.add("show");
 
-        let scoreDisplay = document.createElement("p");
+        var scoreDisplay = document.createElement("p");
         scoreDisplay.innerText = ("Your final score is " + score + "!");
         containerScoreEl.appendChild(scoreDisplay);
     }       
-
+    
+    //create high score values
     let createHighScore = function(event) { 
         event.preventDefault() 
         let initials = document.querySelector("#initials").value;
@@ -214,7 +223,7 @@ let answerWrong = function() {
     }
     //create elements in order of high scores
     for (var i = 0; i < HighScores.length; i++) {
-      var highscoreEl = document.createElement("li");
+      let highscoreEl = document.createElement("li");
       highscoreEl.ClassName = "high-score";
       highscoreEl.innerHTML = HighScores[i].initials + " - " + HighScores[i].score;
       listHighScoreEl.appendChild(highscoreEl);
@@ -224,15 +233,15 @@ let answerWrong = function() {
       displayHighScores();
 
     }
-
-    var saveHighScore = function () {
+    //save high score
+    let saveHighScore = function () {
         localStorage.setItem("HighScores", JSON.stringify(HighScores))
             
     }
 
     //load values/ called on page load
-    var loadHighScore = function () {
-        var LoadedHighScores = localStorage.getItem("HighScores")
+    let loadHighScore = function () {
+        let LoadedHighScores = localStorage.getItem("HighScores")
             if (!LoadedHighScores) {
             return false;
         }
@@ -253,7 +262,7 @@ let answerWrong = function() {
     }  
 
     //display high score screen from link or when intiials entered
-    var displayHighScores = function() {
+    let displayHighScores = function() {
 
         containerHighScoresEl.classList.remove("hide");
         containerHighScoresEl.classList.add("show");
@@ -285,7 +294,7 @@ let answerWrong = function() {
         
     }
     //clears high scores
-    var clearScores = function () {
+    let clearScores = function () {
         HighScores = [];
 
         while (listHighScoreEl.firstChild) {
@@ -299,18 +308,12 @@ let answerWrong = function() {
     loadHighScore()
         
       //on start click, start game
-      btnStartEl.addEventListener("click", startGame);
-
-      btnStartEl.addEventListener("click", function() {
-        console.log("Start Quiz button clicked");
-        startGame();
-      });
-
+      btnStartEl.addEventListener("click", startGame)
       //on submit button -- enter or click
-      formInitials.addEventListener("submit", createHighScore);
+      formInitials.addEventListener("submit", createHighScore)
       //when view high-scores is clicked
-      ViewHighScoreEl.addEventListener("click", displayHighScores);
+      ViewHighScoreEl.addEventListener("click", displayHighScores)
       //Go back button
-      btnGoBackEl.addEventListener("click", renderStartPage);
+      btnGoBackEl.addEventListener("click", renderStartPage)
       //clear scores button
-      btnClearScoresEl.addEventListener("click", clearScores);
+      btnClearScoresEl.addEventListener("click", clearScores)
